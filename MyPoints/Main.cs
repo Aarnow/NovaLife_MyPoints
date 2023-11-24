fad862455f3e2b7dead18e95f6597a17a69cd389
Aarnow <arnaudfgr.dev@gmail.com>
2023-11-25 00:10:47 +0100

    Initial Project Files

---
+++ MyPoints/Main.cs
@@ -0,0 +1,19 @@
+﻿using Life;
+using System;
+
+namespace MyPoints
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+            Console.WriteLine($"Plugin \"MyPoints\" initialisé avec succès.");
+        }
+    }
+}
