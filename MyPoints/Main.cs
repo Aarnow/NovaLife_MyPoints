550aa734f80658f22190fd7713f7961fcd45dfe5
Aarnow <arnaudfgr.dev@gmail.com>
2023-11-25 12:27:36 +0100

    MyPoints Command

--- MyPoints/Main.cs
+++ MyPoints/Main.cs
@@ -1,5 +1,8 @@
 ﻿using Life;
+using Life.CheckpointSystem;
+using Life.Network;
 using System;
+using UnityEngine;
 
 namespace MyPoints
 {
@@ -12,6 +15,19 @@ namespace MyPoints
         public override void OnPluginInit()
         {
             base.OnPluginInit();
+
+            new SChatCommand("/mypoints", "Permet d'ouvrir le panel du plugin MyPoints", "/mypoints", (player, arg) =>
+                {
+
+                    Vector3 pos = player.setup.transform.position;
+                    NCheckpoint newCheckpoint = new NCheckpoint(player.setup.areaId, pos, delegate
+                    {
+                        Console.WriteLine("Bienvenue dans le checkpoint !");
+                    });
+
+                    player.CreateCheckpoint(newCheckpoint);
+
+                }).Register();
 
             Console.WriteLine($"Plugin \"MyPoints\" initialisé avec succès.");
         }
