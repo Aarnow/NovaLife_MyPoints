6408df5340dafe97bb79e69fc08291a87f589c35
Aarnow <arnaudfgr.dev@gmail.com>
2023-12-12 12:38:16 +0100

    fix: weaponShopItems & clothesShopItems

--- MyPoints/Components/ShopPoint/ShopItem.cs
+++ MyPoints/Components/ShopPoint/ShopItem.cs
@@ -40,8 +40,12 @@ namespace MyPoints.Components.ShopPoint
         {
             int[] foodItemIDs = { 1, 2 };
             int[] mechanicItemIDs = { 3, 4, 5 };
+            int[] clothItemIDs = { 85, 102, 103, 104, 105, 106, 107, 125, 126, 153, 154, 1073, 1074, 1127, 1128, 1129, 1130};
+            int[] weaponItemIDs = { 6, 1622, 1629 };
             if (foodItemIDs.Contains(itemId)) return "{\"cookedPercentage\":0,\"expireTimestamp\":0}";
             else if (mechanicItemIDs.Contains(itemId)) return "{\"statePercentage\":100.0}";
+            else if (clothItemIDs.Contains(itemId)) return "";
+            else if (weaponItemIDs.Contains(itemId)) return "{\"currentAmmo\":0}";
             else return null;
         }
     }
